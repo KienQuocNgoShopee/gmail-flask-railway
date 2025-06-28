60b3cef59e7246918be0ed12d6ec8741bf765401
Ngo Kien Quoc_SPXVN17829 [SPXVN17829] <kienquoc.ngo@shopee.com>
2025-06-28 10:16:55 +0700

    28_6_check_token_1

--- app.py
+++ app.py
@@ -25,9 +25,14 @@ db = firestore.client()
 # --- GOOGLE OAUTH SCOPES ---
 SCOPES = [
     "https://www.googleapis.com/auth/gmail.send",
+    "https://www.googleapis.com/auth/gmail.readonly",
+    "https://www.googleapis.com/auth/gmail.compose",
+    "https://www.googleapis.com/auth/gmail.modify",
     "https://www.googleapis.com/auth/userinfo.email",
+    "https://www.googleapis.com/auth/drive.readonly",
     "openid"
 ]
+
 
 # --- TRạNG THÁI ---
 status = {"running": False, "message": "Chưa chạy"}
